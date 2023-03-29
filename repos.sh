9e58549a63ecf9a36398da4476435389daeaa500
Dan Tovbein <dtovbein@gmail.com>
2023-03-29 06:03:48 -0300

    Update cloned branch of theme and plugin
    - Previously using upgrade-node for testing purpose

--- repos.sh
+++ repos.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -ea
 
-BASE_BRANCH="upgrade-node"
-
 if [[ "${GIT_PROTO}" = "ssh" ]]
 then
   GIT_DOMAIN="git@github.com:"
@@ -10,6 +8,6 @@ else
   GIT_DOMAIN="https://github.com/"
 fi
 
-git clone --branch ${BASE_BRANCH} ${GIT_DOMAIN}greenpeace/planet4-master-theme.git persistence/app/public/wp-content/themes/planet4-master-theme
+git clone ${GIT_DOMAIN}greenpeace/planet4-master-theme.git persistence/app/public/wp-content/themes/planet4-master-theme
 
-git clone --branch ${BASE_BRANCH} --recurse-submodules --shallow-submodules ${GIT_DOMAIN}greenpeace/planet4-plugin-gutenberg-blocks.git persistence/app/public/wp-content/plugins/planet4-plugin-gutenberg-blocks
+git clone --recurse-submodules --shallow-submodules ${GIT_DOMAIN}greenpeace/planet4-plugin-gutenberg-blocks.git persistence/app/public/wp-content/plugins/planet4-plugin-gutenberg-blocks
